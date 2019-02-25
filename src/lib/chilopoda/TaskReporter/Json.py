3ea709d3579f06a995e43f9d5e60f3df4092b7db
Paulo Nogueira <paulondc@gmail.com>
2019-02-24 19:59:03 -0800

    added json reporter

---
+++ src/lib/chilopoda/TaskReporter/Json.py
@@ -0,0 +1,43 @@
+import sys
+import json
+from .TaskReporter import TaskReporter
+
+class Json(TaskReporter):
+    """
+    Implements a json task reporter.
+    """
+
+    def display(self):
+        """
+        Implement the json display.
+        """
+        result = {
+            'task': self.taskName(),
+            'execution': int(self.totalTime()),
+            'crawlers': []
+        }
+
+        for crawler in self.crawlers():
+            result['crawlers'].append(
+                {
+                    'crawlerType': crawler.var('type'),
+                    'fullPath': crawler.var('fullPath')
+                }
+            )
+
+        sys.stdout.write('{}\n'.format(
+                json.dumps(
+                    result,
+                    sort_keys=True,
+                    indent=4,
+                    separators=(',', ': ')
+                )
+            )
+        )
+
+
+# registering reporter
+TaskReporter.register(
+    'json',
+    Json
+)
---
+++ src/lib/chilopoda/TaskReporter/__init__.py
@@ -0,0 +1,4 @@
+from .TaskReporter import TaskReporter
+from .Detailed import Detailed
+from .Columns import Columns
+from .Json import Json
