427eeecea0be743e0ffd3c026f527cc436210195
Paulo Nogueira <paulondc@gmail.com>
2019-02-27 20:23:47 -0800

    improved printing columns

--- src/lib/chilopoda/TaskReporter/Columns.py
+++ src/lib/chilopoda/TaskReporter/Columns.py
@@ -6,25 +6,42 @@ class Columns(TaskReporter):
     Implements columns task reporter.
     """
 
+    __columnSize = 8
+    __reserveColumns = 4
+
     def display(self):
         """
         Implement the column display.
         """
-        # computing the crawler type column size
-        crawlerTypeSize = 0
+        # computing task name column size
+        taskNameWidth = self.__columnSize * self.__reserveColumns
+        if self.__textColumnSize(self.taskName()) + self.__columnSize >= taskNameWidth:
+            taskNameWidth = self.__textColumnSize(self.taskName()) + self.__columnSize
+
+        # computing crawler type column size
+        crawlerTypeWidth = self.__columnSize * self.__reserveColumns
         for crawler in self.crawlers():
-            crawlerTypeSize = max(len(crawler.var('type')) + 1, crawlerTypeSize)
+            if self.__textColumnSize(crawler.var('type')) + self.__columnSize >= crawlerTypeWidth:
+                crawlerTypeWidth = self.__textColumnSize(crawler.var('type')) + self.__columnSize
 
         # printing columns
         for crawler in self.crawlers():
             sys.stdout.write(
-                '{} {}{}{}\n'.format(
+                '{}{}{}{}{}\n'.format(
                     self.taskName(),
+                    "\t" * ((taskNameWidth - self.__textColumnSize(self.taskName())) / self.__columnSize),
                     crawler.var('type'),
-                    " " * (crawlerTypeSize - len(crawler.var('type'))),
+                    "\t" * ((crawlerTypeWidth - self.__textColumnSize(crawler.var('type'))) / self.__columnSize),
                     crawler.var('fullPath')
                 )
             )
+
+    @classmethod
+    def __textColumnSize(cls, text):
+        """
+        Return the size of the text fitted in columns.
+        """
+        return len(text) + cls.__columnSize - (len(text) % cls.__columnSize)
 
 
 # registering reporter
