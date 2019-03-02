52f9536596e6319381e4a185f35f361eb32e919d
Paulo Nogueira <paulondc@gmail.com>
2019-03-02 10:54:31 -0800

    no longer necessary

--- tests/Task/Image/ResizeImageTest.py
+++ tests/Task/Image/ResizeImageTest.py
@@ -8,7 +8,6 @@ class ResizeImageTest(BaseTestCase):
     """Test ResizeImage task."""
 
     __sourcePath = os.path.join(BaseTestCase.dataTestsDirectory(), "testSeq.0001.exr")
-    __testPath = os.path.join(BaseTestCase.dataTestsDirectory(), "resizeImage.jpg")
     __targetPath = os.path.join(BaseTestCase.tempDirectory(), "testToDelete.jpg")
 
     def testResizeImage(self):
