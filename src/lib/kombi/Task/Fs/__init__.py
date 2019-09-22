13acb5c06ea769f76873456a08afe1040c12c51c
Paulo Nogueira <paulondc@gmail.com>
2019-09-22 16:07:17 -0700

    added glob task

--- src/lib/kombi/Task/Fs/__init__.py
+++ src/lib/kombi/Task/Fs/__init__.py
@@ -1,5 +1,6 @@
 from .ChecksumTask import ChecksumTask, ChecksumTaskMatchError
 from .RemoveTask import RemoveTask
 from .ChmodTask import ChmodTask
+from .GlobTask import GlobTask
 from .CopyTask import CopyTask, CopyTaskTargetDirectoryError
 from .LinkTask import LinkTask, LinkTaskTargetDirectoryError
