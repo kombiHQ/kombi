491db883abff33b9ca6ed984590306d78c934a19
Paulo Nogueira <paulondc@gmail.com>
2025-01-10 12:47:19 -0800

    emitting changed signal when task holder is executed

--- src/lib/kombiqt/Element/ElementListWidget.py
+++ src/lib/kombiqt/Element/ElementListWidget.py
@@ -390,7 +390,7 @@ class ElementListWidget(QtWidgets.QTreeWidget):
         Slog triggered by the context menu action to run the task holders.
         """
         if RunTaskHoldersWidget.run([self.__taskHolders[index]], elements, parent=self):
-            self.refresh(force=True)
+            self.changed.emit()
 
     def __updateSourceColumns(self, taskHolders):
         """
