8b9d1a7a70f4f43596f08dea17405e144ad4d2c1
Paulo Nogueira <paulondc@gmail.com>
2025-01-30 19:28:44 -0800

    added fallback to pyside2 when Qt is not available

--- src/lib/kombi/TaskWrapper/MayaTaskWrapper.py
+++ src/lib/kombi/TaskWrapper/MayaTaskWrapper.py
@@ -88,7 +88,11 @@ class MayaTaskWrapper(DCCTaskWrapper):
         # when waitSeconds is non-zero, schedule __executeWhenIsIdle to run after the specified delay
         # (in seconds) using QTimer.singleShot, deferring execution until the app is idle
         if waitSeconds != 0:
-            from Qt import QtCore
+            try:
+                from Qt import QtCore
+            # for systems where Qt is not available, we fall back to PySide2. 
+            except ImportError:
+                from PySide2 import QtCore
             QtCore.QTimer.singleShot(waitSeconds * 1000, functools.partial(utils.executeDeferred, __executeWhenIsIdle))
         else:
             utils.executeDeferred(__executeWhenIsIdle)
