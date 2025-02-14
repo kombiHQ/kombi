33337d7f28d546c8d70455c5117033574b5f7073
Paulo Nogueira <paulondc@gmail.com>
2025-02-14 10:26:03 -0700

    Added support for element preview

--- src/lib/kombiqt/OptionVisual/ElementOptionVisual.py
+++ src/lib/kombiqt/OptionVisual/ElementOptionVisual.py
@@ -1,12 +1,14 @@
 from Qt import QtWidgets
 from .OptionVisual import OptionVisual
 from ..Element.ElementsLevelNavigationWidget import ElementsLevelNavigationWidget
+from ..Element.ElementViewer import ElementViewer
 from kombi.Element import Element
 
 class ElementOptionVisual(OptionVisual):
     """
     Implement the widget for an element option.
     """
+    __defaultPreviewHeight = 240
 
     def __init__(self, optionName, optionValue, uiHints=None):
         """
@@ -25,9 +27,27 @@ class ElementOptionVisual(OptionVisual):
         self.setLayout(mainLayout)
 
         self.__mainWidget = ElementsLevelNavigationWidget(showBookmarks=False)
-        self.__mainWidget.setElements([self.optionValue()])
+        if self.optionValue():
+            self.__mainWidget.setElements([self.optionValue()])
+
+        # preview support
+        if self.uiHints().get('preview'):
+            previewTag = self.uiHints().get('previewTag', 'previewFilePath')
+            elementViewerWidget = ElementViewer([], previewTag)
+
+            # by default lets it fill the whole width
+            width = self.uiHints().get('previewWidth', 0)
+            if width:
+                elementViewerWidget.setFixedWidth(width)
+
+            height = self.uiHints().get('previewHeight', self.__defaultPreviewHeight)
+            elementViewerWidget.setFixedHeight(height)
+            if self.optionValue():
+                elementViewerWidget.setElements([self.optionValue()])
+            mainLayout.addWidget(elementViewerWidget, 100)
         mainLayout.addWidget(self.__mainWidget)
 
 
 OptionVisual.register('element', ElementOptionVisual)
 OptionVisual.registerFallbackDefaultVisual('element', Element)
+OptionVisual.registerExample('element', 'preview', None, {'preview': True, 'previewHeight': 180})
