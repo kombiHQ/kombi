4f369643dab953854ceb819b7991155f83bd2cbe
Paulo Nogueira <paulondc@gmail.com>
2025-02-28 16:37:55 -0700

    Add script editor launcher

--- src/lib/kombiqt/__init__.py
+++ src/lib/kombiqt/__init__.py
@@ -8,7 +8,22 @@ from .App import App
 
 def init():
     """
-    Initialize the qt app.
+    Initialize the default kombi app.
     """
     app = App(sys.argv)
+    window = Window.MainWindow.run(sys.argv)
+    window.show()
+    window.activateWindow()
+    sys.exit(app.exec_())
+
+def initScriptEditor():
+    """
+    Initialize the script editor app.
+    """
+    app = App(sys.argv)
+    window = Window.ScriptEditorWindow(
+        rootPath=sys.argv[1] if len(sys.argv) > 1 else ''
+    )
+    window.show()
+    window.activateWindow()
     sys.exit(app.exec_())
