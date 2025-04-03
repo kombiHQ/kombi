566001a34b21234419b4bfcc67cd2dafb99683d2
Paulo Nogueira <paulondc@gmail.com>
2025-04-02 20:18:54 -0600

    Add support for minimum height UI hint

--- src/lib/kombiqt/OptionVisual/ElementArrayOptionVisual.py
+++ src/lib/kombiqt/OptionVisual/ElementArrayOptionVisual.py
@@ -23,17 +23,31 @@ class ElementArrayOptionVisual(OptionVisual):
         mainLayout.setContentsMargins(2, 2, 2, 2)
         self.setLayout(mainLayout)
 
+        # creating a container for the element list widget
+        frameLayout = QtWidgets.QHBoxLayout()
+        frameLayout.setContentsMargins(0, 0, 0, 0)
+        frameWidget = QtWidgets.QFrame()
+        frameWidget.setObjectName('optionVisualContainer')
+        frameWidget.setLayout(frameLayout)
+
         checkableState = self.uiHints().get('checkableState', False)
         elementVarColumnNames = self.uiHints().get('elementVarColumnNames', [])
         self.__mainWidget = ElementListWidget(
             checkableState=checkableState,
             elementVarColumnNames=elementVarColumnNames
         )
+
+        # setting the minimumHeight ui hint when defined
+        minimumHeight = self.uiHints().get('minimumHeight', 600)
+        if minimumHeight:
+            self.__mainWidget.setMinimumHeight(minimumHeight)
+
         if self.optionValue():
             self.__mainWidget.setElements(self.optionValue())
 
         self.__mainWidget.checkedStateChanged.connect(self.__onCheckedStateChanged)
-        mainLayout.addWidget(self.__mainWidget)
+        frameLayout.addWidget(self.__mainWidget)
+        mainLayout.addWidget(frameWidget)
 
         # when checkable state is false we update the current value
         # to reflect an empty list
@@ -52,3 +66,4 @@ OptionVisual.register('elementArray', ElementArrayOptionVisual)
 OptionVisual.registerExample('elementArray', 'checkboxOn', [], {'checkableState': True})
 OptionVisual.registerExample('elementArray', 'checkboxOff', [], {'checkableState': False})
 OptionVisual.registerExample('elementArray', 'columns', [], {'elementVarColumnNames': ['ext', 'type']})
+OptionVisual.registerExample('elementArray', 'minimumHeight', [], {'minimumHeight': 500})
