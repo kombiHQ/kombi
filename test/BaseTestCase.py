52475e9f575d799046aee99321af3acf9beca7fe
Paulo Nogueira <paulondc@gmail.com>
2025-04-10 22:16:41 -0600

    Automatically create and remove temporary directory for the tests

--- test/BaseTestCase.py
+++ test/BaseTestCase.py
@@ -1,8 +1,9 @@
 import os
+import sys
+import shutil
+import fnmatch
 import tempfile
 import unittest
-import fnmatch
-import sys
 
 # querying root directory
 root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -46,6 +47,8 @@ class BaseTestCase(unittest.TestCase):
         """
         if cls.__tempDirectory is None:
             cls.__tempDirectory = tempfile.mkdtemp()
+
+        os.makedirs(cls.__tempDirectory, exist_ok=True)
         return cls.__tempDirectory
 
     @classmethod
@@ -71,3 +74,17 @@ class BaseTestCase(unittest.TestCase):
             os.access(os.path.join(path, binName), os.X_OK)
             for path in os.environ["PATH"].split(os.pathsep)
         )
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Create the temporary directory used by the tests.
+        """
+        os.environ['KOMBI_TEMP_REMOTE_DIR'] = cls.tempDirectory()
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Remove temporary files.
+        """
+        shutil.rmtree(cls.tempDirectory(), ignore_errors=True)
