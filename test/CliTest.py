8c3189cd33be2626290ad6c8a6099a70bedcbc7c
Paulo Nogueira <paulondc@gmail.com>
2025-04-26 15:23:02 -0600

    Update cli test data

--- test/CliTest.py
+++ test/CliTest.py
@@ -5,40 +5,46 @@ from fnmatch import fnmatch
 from .BaseTestCase import BaseTestCase
 from kombi.Cli import Cli
 from kombi.TaskHolder.Loader import JsonLoader
-from kombi.ResourceLoader import ResourceLoader
 
 class CliTest(BaseTestCase):
     """Test for cli app."""
 
     __jsonConfig = os.path.join(BaseTestCase.dataTestsDirectory(), 'config', 'reporterTest.json')
-    __taskPath = os.path.join(BaseTestCase.dataTestsDirectory(), 'tasks', 'EchoTask.py')
     __output = """
-        echoTask\t\ttestElement\t\t*/testSeq.0001.exr
-        echoTask\t\ttestElement\t\t*/testSeq.0002.exr
-        echoTask\t\ttestElement\t\t*/testSeq.0003.exr
-        echoTask\t\ttestElement\t\t*/testSeq.0004.exr
-        echoTask\t\ttestElement\t\t*/testSeq.0005.exr
-        echoTask\t\ttestElement\t\t*/testSeq.0006.exr
-        echoTask\t\ttestElement\t\t*/testSeq.0007.exr
-        echoTask\t\ttestElement\t\t*/testSeq.0008.exr
-        echoTask\t\ttestElement\t\t*/testSeq.0009.exr
-        echoTask\t\ttestElement\t\t*/testSeq.0010.exr
-        echoTask\t\ttestElement\t\t*/testSeq.0011.exr
-        echoTask\t\ttestElement\t\t*/testSeq.0012.exr
-        echoTask\t\tjpg\t\t\t*/testSeq.jpg
-        echoTask\t\tturntable\t\t*/RND_ass_lookdev_default_beauty_tt.1001.exr
-        echoTask\t\tnukeRender\t\t*/RND-TST-SHT_comp_compName_output_v010_tk.1001.exr
-        echoTask\t\tshotRender\t\t*/RND-TST-SHT_lighting_beauty_sr.1001.exr
-        echoTask\t\ttestElement\t\t*/test_0001.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0001.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0002.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0003.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0004.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0005.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0006.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0007.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0008.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0009.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0010.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0011.exr
+        convertImage\t\ttestElement\t\t*/testSeq.0012.exr
+        convertImage\t\tjpg\t\t\t*/testSeq.0001.jpg
+        convertImage\t\tjpg\t\t\t*/testSeq.0002.jpg
+        convertImage\t\tjpg\t\t\t*/testSeq.0003.jpg
+        convertImage\t\tjpg\t\t\t*/testSeq.0004.jpg
+        convertImage\t\tjpg\t\t\t*/testSeq.0005.jpg
+        convertImage\t\tjpg\t\t\t*/testSeq.0006.jpg
+        convertImage\t\tjpg\t\t\t*/testSeq.0007.jpg
+        convertImage\t\tjpg\t\t\t*/testSeq.0008.jpg
+        convertImage\t\tjpg\t\t\t*/testSeq.0009.jpg
+        convertImage\t\tjpg\t\t\t*/testSeq.0010.jpg
+        convertImage\t\tjpg\t\t\t*/testSeq.0011.jpg
+        convertImage\t\tjpg\t\t\t*/testSeq.0012.jpg
+        convertImage\t\tturntable\t\t*/RND_ass_lookdev_default_beauty_tt.1001.exr
+        convertImage\t\tnukeRender\t\t*/RND-TST-SHT_comp_compName_output_v010_tk.1001.exr
+        convertImage\t\tshotRender\t\t*/RND-TST-SHT_lighting_beauty_sr.1001.exr
+        convertImage\t\ttestElement\t\t*/test_0001.exr
     """
 
     def testOutput(self):
         """
         Test the output produced by the cli app.
         """
-        resource = ResourceLoader.get()
-        resource.load(self.__taskPath)
-
         taskHolderLoader = JsonLoader()
         taskHolderLoader.loadFromFile(self.__jsonConfig)
 
