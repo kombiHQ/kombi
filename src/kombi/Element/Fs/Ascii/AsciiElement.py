ee93f1e0f338d12fa8a602efb3564a57bdd62148
Paulo Nogueira <paulondc@gmail.com>
2025-06-27 12:56:54 -0700

    Add support for details tag

--- src/kombi/Element/Fs/Ascii/AsciiElement.py
+++ src/kombi/Element/Fs/Ascii/AsciiElement.py
@@ -1,9 +1,12 @@
+import os
+import sys
 from ..FileElement import FileElement
 
 class AsciiElement(FileElement):
     """
     Abstracted ascii element.
     """
+    __detailsMaxSize = int(os.environ.get('KOMBI_ELEMENT_DETAILS_MAX_SIZE', 10 * 1024))
 
     def __init__(self, *args, **kwargs):
         """
@@ -14,8 +17,13 @@ class AsciiElement(FileElement):
 
         # setting icon
         self.setTag('icon', 'icons/elements/ascii.png')
-
         self.setVar('category', 'ascii')
+
+        # the 'details' tag is used to show additional information when an element is selected
+        # and the browser's info panel is activated. It should be set to the name of a method (as a string).
+        # If the method returns a QWidget, that widget is used as the details view. Otherwise, a default
+        # OptionValue widget is automatically generated to display the data.
+        self.setTag('details', 'computeDetails')
 
     def _runParser(self):
         """
@@ -34,3 +42,25 @@ class AsciiElement(FileElement):
             self.__parsedContents = self._runParser()
 
         return self.__parsedContents
+
+    def computeDetails(self):
+        """
+        Compute the details content display in the browser.
+        """
+        dataSize = self.dataSize(self.contents())
+        if dataSize <= self.__detailsMaxSize:
+            return self.contents()
+
+        return f"Cannot display details (exceeds {self.__detailsMaxSize} bytes)."
+
+    @classmethod
+    def dataSize(cls, data):
+        """
+        Utility method to return the data size in bytes.
+        """
+        size = sys.getsizeof(data)
+        if isinstance(data, dict):
+            size += sum(cls.dataSize(key) + cls.dataSize(value) for key, value in data.items())
+        elif isinstance(data, (list, tuple, set)):
+            size += sum(cls.dataSize(item) for item in data)
+        return size
