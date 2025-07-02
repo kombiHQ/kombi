ad59d80c362e0aefd975aac27e0ff51475d14f1c
Paulo Nogueira <paulondc@gmail.com>
2025-07-02 14:50:25 -0700

    Add support for byteSize, owner and modificationDate

--- src/kombi/Element/Fs/FileElement.py
+++ src/kombi/Element/Fs/FileElement.py
@@ -1,10 +1,50 @@
+import platform
+from datetime import datetime
 from .FsElement import FsElement
 from ..Element import Element
+
+if platform.system() == 'Windows':
+    import ctypes
+    from ctypes import wintypes
+else:
+    import pwd
+    import grp
 
 class FileElement(FsElement):
     """
     File element.
     """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Create a file element object.
+        """
+        super().__init__(*args, **kwargs)
+
+        self.setTag('details', 'computeDetails')
+
+    def var(self, name, *args, **kwargs):
+        """
+        Return var value using lazy loading implementation for owner, byteSize and modificationDate.
+        """
+        if name in ('owner', 'byteSize', 'modificationDate') and name not in self.varNames():
+            stat = self.path().stat()
+            modificationDate = datetime.fromtimestamp(stat.st_mtime)
+            self.setVar('byteSize', stat.st_size)
+            self.setVar('modificationDate', modificationDate.strftime('%Y-%m-%d %H:%M:%S'))
+            self.setVar('owner', self.__computeOwner(stat))
+
+        return super().var(name, *args, **kwargs)
+
+    def computeDetails(self):
+        """
+        Return the information displayed in the Details panel when requested.
+        """
+        return {
+            "sizeMb": self.var('byteSize') / (1024 ** 2),
+            "modificationDate": self.var('modificationDate'),
+            "owner": self.var('owner')
+        }
 
     @classmethod
     def test(cls, path, parentElement):
@@ -14,6 +54,68 @@ class FileElement(FsElement):
         if not super(FileElement, cls).test(path, parentElement):
             return False
         return cls.cachedPathQuery(path, 'is_file')
+
+    def __computeOwner(self, stat):
+        """
+        Return the file owner.
+
+        If the user is part of a group|domain, the result is in the format <group|domain>/<user>.
+        """
+        if platform.system() == 'Windows':
+            return self.__computeOwnerWindows()
+
+        # posix
+        owner = pwd.getpwuid(stat.st_uid).pw_name
+        group = grp.getgrgid(stat.st_gid).gr_name
+        return f'{group}/{owner}' if group else owner
+
+    def __computeOwnerWindows(self):
+        """
+        Return the file owner on Windows.
+
+        If the user is part of a domain, the result is in the format <domain>/<user>.
+        """
+        lengthNeeded = wintypes.DWORD(0)
+        ctypes.windll.advapi32.GetFileSecurityW(
+            self.var('filePath'),
+            0x00000001,
+            None,
+            0,
+            ctypes.byref(lengthNeeded)
+        )
+
+        securityDescriptor = ctypes.create_string_buffer(lengthNeeded.value)
+        if not ctypes.windll.advapi32.GetFileSecurityW(
+            self.var('filePath'),
+            0x00000001,
+            securityDescriptor,
+            lengthNeeded.value,
+            ctypes.byref(lengthNeeded)
+        ):
+            return ctypes.WinError()
+
+        ownerId = ctypes.c_void_p()
+        if not ctypes.windll.advapi32.GetSecurityDescriptorOwner(
+            securityDescriptor,
+            ctypes.byref(ownerId),
+            ctypes.byref(wintypes.BOOL())
+        ):
+            raise ctypes.WinError()
+
+        name = ctypes.create_unicode_buffer(256)
+        domain = ctypes.create_unicode_buffer(256)
+        if not ctypes.windll.advapi32.LookupAccountSidW(
+            None,
+            ownerId,
+            name,
+            ctypes.byref(wintypes.DWORD(len(name))),
+            domain,
+            ctypes.byref(wintypes.DWORD(len(domain))),
+            ctypes.byref(wintypes.DWORD())
+        ):
+            raise ctypes.WinError()
+
+        return f'{domain.value}/{name.value}' if domain.value else name.value
 
 
 Element.register(
